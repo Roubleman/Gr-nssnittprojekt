9dde8ba2dbeca6b82cc4bfd45d0338544d13f15a
Ihrefjord <elias@ihrefjord.com>
2023-11-26 09:51:10 +0100

    Index router fixed för 2 lobbyviews

--- src/router/index.js
+++ src/router/index.js
@@ -1,40 +1,40 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import StartView from '../views/StartView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import StartView from "../views/StartView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'Start',
-      component: StartView
+      path: "/",
+      name: "Start",
+      component: StartView,
     },
     {
-      path: '/game/:id',
-      name:  'GameView',
-      component: () => import('../views/GameView.vue')
-    },
-      {
-      path: '/host/:id',
-      name: 'HostLobbyView',
-      component: () => import('../views/HostLobbyView.vue')
+      path: "/game/:id",
+      name: "GameView",
+      component: () => import("../views/GameView.vue"),
     },
     {
-      path: '/join/:id',
-      name: 'JoinLobbyView',
-      component: () => import('../views/PlayerLobbyView.vue')
-    }
-    {
-      path: '/create/',
-      name: 'CreateView',
-      component: () => import('../views/CreateView.vue')
+      path: "/host/:id",
+      name: "HostLobbyView",
+      component: () => import("../views/HostLobbyView.vue"),
     },
     {
-      path: '/result/:id',
-      name: 'ResultView',
-      component: () => import('../views/ResultView.vue')
-    }
-  ]
-})
+      path: "/join/:id",
+      name: "PlayerLobbyView",
+      component: () => import("../views/PlayerLobbyView.vue"),
+    },
+    {
+      path: "/create/",
+      name: "CreateView",
+      component: () => import("../views/CreateView.vue"),
+    },
+    {
+      path: "/result/:id",
+      name: "ResultView",
+      component: () => import("../views/ResultView.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
