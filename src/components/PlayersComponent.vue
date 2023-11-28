4a916832782f6885245c9e20b57809f78c605372
gustavmolin <gustav.molin.8810@student.uu.se>
2023-11-28 16:51:27 +0100

    test playersComp

---
+++ src/components/PlayersComponent.vue
@@ -0,0 +1,36 @@
+<template>
+    <header id="player_header"> Your turn!</header>
+    <div class="player">
+        <OneCard :card="currentCard" width="8em" height="8em" />
+        <button @click="guess('higher')">Higher</button>
+        <button @click="guess('lower')">Lower</button>
+    </div>
+</template>
+  
+<script>
+import OneCard from "@/components/OneCard.vue";
+
+export default {
+    name: "Player",
+    components: {
+        OneCard,
+    },
+    props: {
+        currentCard: {
+            type: Object,
+            required: true,
+        },
+    },
+    methods: {
+        guess(direction) {
+            this.$emit('guess', direction);
+        },
+    },
+};
+</script>
+<style>
+#player_header {
+    background-color: beige;
+    margin-bottom: 3em;
+}
+</style>
