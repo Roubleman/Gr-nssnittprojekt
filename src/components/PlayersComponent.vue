75591f7428ab4044458d8455170e14c65c68b2f6
gustavmolin <gustav.molin.8810@student.uu.se>
2023-11-30 10:54:50 +0100

    adderat metoder

--- src/components/PlayersComponent.vue
+++ src/components/PlayersComponent.vue
@@ -1,17 +1,25 @@
 <template>
     <h1>It's your turn! Guess a Card</h1>
+
+
     <section id="cardSelection">
-
-
-
-
         <div class="card-grid">
+
+            <OneCard v-for="card in cards" :card="card" :key="card.suit + card.value"
+                :style="{ 'grid-row-start': getRow(card.value), 'grid-column-start': getColumn(card.value), 'z-index': getZIndex(card.suit) }">
+            </OneCard>
+
+
+
+
+            <!--
             <div v-for="(group, value) in cardGroups" :key="value">
                 <OneCard v-for="(card, index) in group" :card="card" :key="card.suit"
                     :style="{ transform: `translateY(${index * -155}px)` }" v-on:selectedCard="selectCard($event)"
                     :class="{ 'selected-card': isSelected(card) }" width="8em" height="8em" class="no-selection"></OneCard>
 
             </div>
+            -->
         </div>
 
     </section>
@@ -32,6 +40,7 @@ export default {
 
             deckOfCards, // Two rows of cards
             selectedCard: [],
+            cards: [],
         };
     },
     props: {
@@ -39,6 +48,10 @@ export default {
             type: Boolean,
             required: true,
         },
+    },
+
+    created() {
+        this.cards = (this.shuffleCards(deckOfCards));
     },
     methods: {
         selectCard(card) {
@@ -51,9 +64,62 @@ export default {
         isSelected(card) {
             return this.selectedCard.some(c => c.value === card.value && c.suit === card.suit);
         },
+        getColumn(value) {
+            const positions = {
+                "2": 1,
+                "3": 2,
+                "4": 3,
+                "5": 4,
+                "6": 5,
+                "7": 6,
+                "8": 7,
+                "9": 1,
+                "10": 2,
+                "J": 3,
+                "Q": 4,
+                "K": 5,
+                "A": 6,
+            };
+            return positions[value];
+        },
+        getRow(value) {
+            const positions = {
+                "2": 1,
+                "3": 1,
+                "4": 1,
+                "5": 1,
+                "6": 1,
+                "7": 1,
+                "8": 1,
+                "9": 2,
+                "10": 2,
+                "J": 2,
+                "Q": 2,
+                "K": 2,
+                "A": 2,
+            };
+            return positions[value];
+        },
+        shuffleCards: function (deck) {
+            const shuffledDeck = [...deck];
+            for (let i = shuffledDeck.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [shuffledDeck[i], shuffledDeck[j]] = [shuffledDeck[j], shuffledDeck[i]];
+            }
+            return shuffledDeck;
+        },
+        getZIndex(suit) {
+            const suits = {
+                "hearts": 1,
+                "diamonds": 2,
+                "clubs": 3,
+                "spades": 4,
+            };
+            return suits[suit];
 
-    }
-    ,
+
+        },
+    },
     computed: {
         cardGroups() {
             return this.deckOfCards.reduce((groups, card) => {
@@ -72,27 +138,32 @@ export default {
 <style scoped>
 #cardSelection {
     display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(var(--card-width), 1fr));
-    gap: 1em;
-    justify-content: center;
-    --card-width: 8em;
-    margin-left: 2em;
+
+    grid-template-columns: repeat(11, 1fr);
+    grid-template-rows: repeat(2, 1fr);
+    gap: 2em;
+    position: relative;
+    margin-left: 10em;
+
 }
 
 .card-grid {
-    display: grid;
-    grid-template-columns: repeat(7, 1fr);
-    gap: 1em;
-    row-gap: -10em;
+    display: contents;
 }
 
-.card-grid>* {
-    position: relative;
 
-
-}
 
 .selected-card {
     background-color: grey;
+}
+
+.OneCard {
+    position: absolute;
+
+}
+
+.card-border {
+    background-color: lightgray;
+    border: 2px solid grey;
 }
 </style>
